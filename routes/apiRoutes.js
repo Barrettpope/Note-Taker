f95633216d0338741f5f9c163cc423525a122f10
Barrett Pope <barrettpope94@gmail.com>
2020-08-27 23:42:31 -0400

    Added commentary to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,44 +1,19 @@
-// // Including the Path and FS packages & creating an empty array for the user inputs to be stored
-// const fs = require("fs");
-// const path = require("path");
-
-
-// // Routing
-// module.exports = function (app) {
-//     // API GET Request
-//     app.get("/api/notes", function (req, res) {
-//         const notes = JSON.parse(fs.readFile(path.join(__dirname, "../db/db.json", "utf8")));
-//         res.json(notes);
-//     });
-
-//     // API POST Request
-//     app.post("/api/notes", function (req, res) {
-//         const notes = JSON.parse(fs.readFile(path.join(__dirname, "../db/db.json", "utf8")));
-//         const newNote = req.body;
-//         notes.push(newNote);
-
-//         fs.writeFile(path.join(__dirname, "../db/db.json", JSON.stringify(notes)));
-//         res.sendStatus(200);
-//     });
-
-//     // API DELETE Request 
-//     app.delete("/api/notes/:id", function (req, res) {
-//         console.log(req.params.id)
-//     });
-// };
-
+// Including the Path and FS packages & creating an empty array for the user inputs to be stored
 const fs = require('fs')
 const path = require('path')
 const DB_PATH = path.join(__dirname, '../db/db.json')
 
+// // Routing
 module.exports = function (app) {
+
+    // API GET Request
     app.get("/api/notes", function (req, res) {
         const notesData = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
         console.log(notesData)
         res.json(notesData);
     });
 
-
+    // API POST Request
     app.post("/api/notes", function (req, res) {
 
         const notesData = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
@@ -52,6 +27,7 @@ module.exports = function (app) {
         res.sendStatus(200)
     });
 
+    // API DELETE Request
     app.delete("/api/notes/:id", function (req, res) {
         const notes = JSON.parse(fs.readFileSync(DB_PATH, "utf8"));
         const savedNotes = notes.filter((note) => {
