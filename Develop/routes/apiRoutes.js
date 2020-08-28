a37a01d0f4467bff29edb34b339ad14148facf13
Barrett Pope <barrettpope94@gmail.com>
2020-08-27 20:13:59 -0400

    Logic added to api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,33 @@
-var db = require("../db/db.json");
+const fs = require("fs");
+const path = require("path");
+const notes = []
 
 module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
+            if (err) throw err;
+            notes = JSON.parse(data);
+            res.json(JSON.parse(data));
+        });
+    });
 
+    app.post("/api/notes", function (req, res) {
+        let newNote = req.body;
+        newNode.id = notes.length;
+        notes.push(newNote);
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(notes);
+        });
+    });
 
-}
+    app.delete("/api/notes/:id", function (req, res) {
+        notes = notes.filter((note) => {
+            return note.id != req.params.id;
+        });
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(notes);
+        });
+    });
+};
